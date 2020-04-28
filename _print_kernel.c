8771900ba815826240ec8c80b053b92a6a1fe054
Ching-Chu-Lin <b06902111@ntu.edu.tw>
2020-04-28 15:44:41 +0800

    final?

--- _print_kernel.c
+++
@@ -1,7 +0,0 @@
-#include<linux/kernel.h>
-#include<linux/linkage.h>
-
-asmlinkage void _print_kernel(char *dmesg){
-	printk(dmesg);
-	return; 
-}
--- to_output.sh
+++
@@ -1,7 +0,0 @@
-for file in OS_PJ1_Test/*
-do
-    output_fname=`echo $file | awk -F '/' '{print $NF}' | awk -F '.' '{print $1}'`
-    output_fname="${output_fname}_stdout.txt"
-    echo "./main.out < $file > output/$output_fname"
-    ./main.out < $file > output/$output_fname
-done
